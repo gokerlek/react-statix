a97c30fa5958d1fb26a78c657d4c7bc991c079e4
Göker ZAFER <gokerlek@gmail.com>
2025-07-25 12:02:17 +0300

    refactor: replace SVG import with inline SVG in StatixButton component

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { dts } from 'rollup-plugin-dts';
 import { readFileSync } from 'fs';
-import svg from 'rollup-plugin-svg';
 
 const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
 
@@ -29,17 +28,14 @@ export default [
         browser: true,
       }),
       commonjs(),
-      svg(),
       typescript({
         tsconfig: './tsconfig.json',
-        declaration: true,
-        declarationDir: 'dist',
       }),
     ],
     external: ['react', 'react-dom'],
   },
   {
-    input: 'dist/index.d.ts',
+    input: 'src/index.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
